bd64ac5dc0294998bef51f0be10dfe6c6bd7eb4b
utensil <utensilcandel@gmail.com>
2024-05-09 09:45:21 +0800

    Change the expected protocol to HTTP-like `\r\n\r\n`

--- lean4_jupyter/kernel.py
+++ lean4_jupyter/kernel.py
@@ -24,35 +24,43 @@ version_pat = re.compile(r'version (\d+(\.\d+)+(-rc\d+)?)')
 
 # Based on https://github.com/zhangir-azerbayev/pySagredo/blob/main/pysagredo/gym/__init__.py
 class Lean4ReplWrapper:
+    
+
     def __init__(self):
         self.check()
-        self.proc = pexpect.spawn("repl", echo=True, encoding='utf-8', codec_errors='replace')
+        self.repl = pexpect.spawn("repl", echo=False, encoding='utf-8', codec_errors='replace')
         self.env = None
         self.commands = {}
+        self.expect_patterns = self.repl.compile_pattern_list([
+            '\r\n\r\n',
+            # pexpect.EOF,
+            # pexpect.TIMEOUT,
+            # pexpect.ExceptionPexpect
+        ])
 
     def check(self):
         # check if Lean is installed
         try:
             check_output(['lean', '--version'])
         except FileNotFoundError:
-            raise FileNotFoundError("Lean is not installed. Please install Lean before using this kernel.")
-        
+            raise FileNotFoundError("Lean is not installed. Please install Lean before using this kernel.")     
     
     def run_command(self, code, timeout=20):
+        repl = self.repl
         command_dict = {
                 "cmd": code,
                 "env": self.env
         } # [1:-1] removes single quotes
 
         command = json.dumps(command_dict)
-        self.proc.sendline(command)
-        self.proc.expect_exact(command + "\r\n")
+        repl.sendline(command)
+        # repl.expect_exact(command + "\r\n")
 
-        self.proc.sendline()
-        self.proc.expect_exact("\r\n")
+        repl.sendline()
+        # repl.expect_exact("\r\n")
         try:
-            index = self.proc.expect('env": \d+\}', timeout=timeout)
-            output = self.proc.before + self.proc.match.group()
+            index = repl.expect_list(self.expect_patterns, timeout=timeout)
+            output = repl.before # + repl.match.group()
             output_dict = json.loads(output)
             # output_dict['sent_raw'] = command
             output_dict['sent'] = command_dict
@@ -64,13 +72,13 @@ class Lean4ReplWrapper:
 
             return output_dict
         except pexpect.exceptions.TIMEOUT:
-            self.proc.sendintr()
+            repl.sendintr()
             interrupted = True
-            return {"error": "FAILED DUE TO TIMEOUT", "buffer": self.proc.buffer}
+            return {"error": "FAILED DUE TO TIMEOUT", "buffer": repl.buffer}
         except KeyboardInterrupt:
-            return {"error": "FAILED DUE TO KEYBOARD INTERRUPT", "buffer": self.proc.buffer}
+            return {"error": "FAILED DUE TO KEYBOARD INTERRUPT", "buffer": repl.buffer}
         except EOF:
-            return {"error": "FAILED DUE TO EOF", "buffer": self.proc.buffer}
+            return {"error": "FAILED DUE TO EOF", "buffer": repl.buffer}
 
 class Lean4Kernel(Kernel):
     implementation = 'lean4_jupyter'
