23617912c8625159d06de881045f32ed76a9ad7b
utensil <utensilcandel@gmail.com>
2024-05-16 20:30:59 +0800

    Fix path

--- scripts/install_repl.sh
+++ scripts/install_repl.sh
@@ -8,6 +8,6 @@ git clone -b fix-dup https://github.com/utensil/repl .lean4_jupyter/repl
 cd .lean4_jupyter/repl
 lake build
 # rm /usr/local/bin/repl
-ln -s $DIR/../.lake/build/bin/repl /usr/local/bin/repl
+ln -s $DIR/../.lean4_jupyter/repl/.lake/build/bin/repl /usr/local/bin/repl
 ls -lhta /usr/local/bin/repl
 echo '{"cmd": "#eval Lean.versionString"}'|repl
