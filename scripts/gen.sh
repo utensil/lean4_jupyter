89787adfa930f0f67a37bed578631745d33f4091
utensil <utensilcandel@gmail.com>
2024-06-03 17:40:18 +0800

    Add gen.sh

---
+++ scripts/gen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Script purpose: regenerate notebooks
+
+# get script parent directory
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_ROOT=$SCRIPT_DIR/..
+REPL_HOME=$PROJECT_ROOT/.lean4_jupyter/repl
+
+# for all files in examples/ that have the extension .ipynb
+for file in $PROJECT_ROOT/examples/*.ipynb; do
+  # regenerate the notebook # --log-level DEBUG
+  python -m papermill --cwd $PROJECT_ROOT/examples --kernel lean4 $file $file
+done
